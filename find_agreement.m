c91b708805c56a4dc16353147700556fbc403122
Naveen Madapana <naveen.madapana@gmail.com>
2018-02-27 13:19:18 -0500

    Data required for the radiology journal was added to the find_agreement function

--- find_agreement.m
+++ find_agreement.m
@@ -1,5 +1,13 @@
 clear; clc; close all;
 
+%% Initialization
+components = [4]; % 1: Random, 2: context, 3: modifier, 4: c + m, 5: m - c
+
+col_names = {'motion_cols', 'orien_cols', 'state_cols'};
+% col_names = {'act_cols'};
+
+metric = 'jaccard';
+%%
 load('data')
 
 sub_ids = (1:9); % Total number of subjects.
@@ -8,7 +16,6 @@ num_sd = numel(sd_names);
 
 if ~exist('data_final','var')
     X = zeros(num_cmd, num_sd, numel(sub_ids));
-    
     for idx = 1 : numel(sub_ids)
         sub_idx = sub_ids(idx)
         [A, ~, ~] = xlsread(xls_file, sprintf('S (%d)',sub_idx));
@@ -19,11 +26,6 @@ if ~exist('data_final','var')
     data_final = X;
     save('data','data_final','-append')
 end
-
-components = 1:5; % 1: Random, 2: context, 3: modifier, 4: c + m, 5: m - c
-
-col_names = {'motion_cols', 'orien_cols', 'state_cols'};
-% col_names = {'act_cols'};
 
 % sd_cols contains all the column indices to consider
 sd_cols = [];
@@ -37,10 +39,7 @@ ag_values = zeros(num_cmd, numel(components), 3);
 argmax_lexicon_ids = cell(num_cmd, numel(components));
 popularity = zeros(num_cmd, numel(components));
 
-metric = 'jaccard';
-
 for comp_idx = 1:numel(components)
-    comp_idx
     switch components(comp_idx)
         case 1
             fprintf('Random data\n')
@@ -64,7 +63,7 @@ for comp_idx = 1:numel(components)
     end
     
     % Size of Y - 34 x _ x 9. Next line changes the sizes. 
-    Y = permute(Y, [3,2,1]); % 9 x 9 x 34 % Rows are semantic vectors
+    Y = permute(Y, [3,2,1]); % 9 x _ x 34 % Rows are semantic vectors
     
     for cmd_idx = 1 : size(Y, 3)
         % Extract semantic vectors of 9 gestures for one command
@@ -98,12 +97,27 @@ for comp_idx = 1:numel(components)
         % Method 3 : CHI Paper (Our method)
         ag_values(cmd_idx, comp_idx, 3) = loa_semantics(sem_mat_cmd, metric);
     end
+    
+    % Finding the frequency of each descriptor for all commands
+    freq = permute(sum(Y), [3, 2, 1]); % 34 x 55
+    [max_freq, argmax_freq] = sort(freq, 2, 'descend');
+    argmax_freq = argmax_freq(reduced_command_ids,1:10);
+    argmax_sds = sd_names(act_cols(argmax_freq));
+    max_freq = floor(100*max_freq(reduced_command_ids, 1:10)/9);
+    
+end
+
+final_cell = cell(28,10);
+for i = 1 : 28
+   for j = 1 : 10
+       final_cell{i,j} = strcat(num2str(max_freq(i, j)), ' - ', argmax_sds{i, j});
+   end
 end
 
 % Extracting only the results for the following condition:
     % reduced command ids
     % context + modifier case
     % only method 1 and 2
-ag_values = permute(ag_values(reduced_command_ids, 4, :), [1,3,2]);
-argmax_lexicon_ids = argmax_lexicon_ids(reduced_command_ids, 4);
-popularity = popularity(reduced_command_ids, 4);
+% ag_values = permute(ag_values(reduced_command_ids, 4, :), [1,3,2]);
+% argmax_lexicon_ids = argmax_lexicon_ids(reduced_command_ids, 4);
+% popularity = popularity(reduced_command_ids, 4);
